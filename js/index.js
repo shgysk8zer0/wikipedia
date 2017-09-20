479d0958ffbbb07d5d29fcbed22880c791ea3f71
Chris Zuber <shgysk8zer0@gmail.com>
2017-09-20 13:43:36 -0700

    Create skeleton HTML - Add `<form>` & `<template>` - Add submit handler for form, logging submitted data - Import normalize.css as stylesheet

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 import {$} from './std-js/functions.js';
+$(self).ready(() => {
+	$(document.forms.search).submit(async (submit) => {
+		submit.preventDefault();
+		const form = new FormData(submit.target);
+		console.log(...form.entries());
+	});
+}, {once: true});
